5f72100be37d3dcd6a8791cf3826874836fb4f84
unknown <nurbek3349@gmail.com>
2020-06-18 21:04:00 +0300

    initial

---
+++ android/app/src/main/java/com/anew/flutterappss/MainActivity.java
@@ -0,0 +1,6 @@
+package com.anew.flutterappss;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutterappss/home_page.dart';
+
+void main() {
+  //uygulama açıldığı an bu fonklsiyon çalışacak o da MyApp() i çalıştırcak
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      //main.dartta kalabalık olmasın diye HomePage() dosyasına taşıdım ordan devam et
+      home: HomePage()
+    );
+  }
+}
